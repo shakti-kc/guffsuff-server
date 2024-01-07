088de4bc941e188fa21ce698a9c10db49af3bc01
Shakti K.C. <shaktikc63@gmail.com>
2024-01-07 18:14:05 +0545

    for hosting

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const io=new Server(server)
 app.use(router)
 app.use(cookieParser())
 app.use(cors({
-  origin: "https://gufsuff.netlify.app",
+  origin: "https://guff-suff.netlify.app",
   credentials:true,
   methods: "GET,PUT,POST,PATCH,DELETE"
 }))
